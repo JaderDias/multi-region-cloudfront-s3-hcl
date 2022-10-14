61e4b3528f3e5d6887285ace725eac649847b7b6
Jader Dias <52470+JaderDias@users.noreply.github.com>
2022-10-14 16:57:18 +0200

    refactor cloudfront-warmup into a separate project

--- golang/s3update/main.go
+++ golang/s3update/main.go
@@ -2,79 +2,27 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"os"
-	"runtime"
-	"strings"
 
-	"github.com/JaderDias/limiter"
-
-	"example.com/s3update/types"
+	warmup "github.com/JaderDias/cloudfront-warmup"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func Request(domainName, uri string) {
-	domainName = strings.TrimSuffix(domainName, ".cloudfront.net")
-	numberOfWorkers := runtime.NumCPU() * 4 // optimal number for my use case
-	limiter.BoundedConcurrencyWithDoneProcessor(
-		numberOfWorkers,
-		len(cloudfrontPoPs),
-		func(i int) error {
-			pop := cloudfrontPoPs[i]
-			log.Printf("Requesting %s from %s", uri, pop)
-			ips, err := getIPs(domainName, pop)
-			if err != nil {
-				return fmt.Errorf("error looking up %s: %v", pop, err)
-			}
+var (
+	cloudfrontDomainName string
+)
 
-			for _, ip := range ips {
-				if ip.To4() == nil {
-					// AWS Lambda doesn't support IPv6 outgoing connections
-					continue
-				}
-
-				client := GetHttpClient(ip)
-				response, err := client.Get(uri)
-				if err != nil {
-					return fmt.Errorf("request for %s ip %s failed: %s", pop, ip, err)
-				}
-
-				log.Printf(
-					"response for %s status %s x-cache %s x-amz-cf-pop %s",
-					ip,
-					response.Status,
-					response.Header.Get("x-cache"),
-					response.Header.Get("x-amz-cf-pop"),
-				)
-			}
-
-			return nil
-		},
-		func(err error) {
-			if err != nil {
-				log.Printf("error: %s", err)
-			}
-		},
-	)
-}
-
-func HandleRequest(ctx context.Context, event types.LambdaEvent) error {
-	log.Printf("Hello %+v!", event)
-	domainName := os.Getenv("DOMAIN_NAME")
-	if len(event.Records) == 0 {
-		log.Fatalf("no records")
-	}
-
-	for _, record := range event.Records {
-		uri := fmt.Sprintf("https://%s/%s", domainName, record.S3.Object.Key)
-		log.Printf("uri: %s", uri)
-		Request(domainName, uri)
-	}
-
-	return nil
+func HandleRequest(ctx context.Context, event events.S3Event) error {
+	return warmup.Warmup(cloudfrontDomainName, event)
 }
 
 func main() {
+	cloudfrontDomainName = os.Getenv("DOMAIN_NAME")
+	if cloudfrontDomainName == "" {
+		log.Fatalf("DOMAIN_NAME not set")
+	}
+
 	lambda.Start(HandleRequest)
 }
