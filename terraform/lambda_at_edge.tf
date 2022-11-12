6e24e951cc99dfb2afbe56cc3ee4b31e3f87a345
Jader Dias <52470+JaderDias@users.noreply.github.com>
2022-11-12 22:06:49 +0100

    skip using s3 to upload code

--- terraform/lambda_at_edge.tf
+++ terraform/lambda_at_edge.tf
@@ -1,9 +1,8 @@
 module "lambda-at-edge" {
   source                 = "JaderDias/lambda-at-edge/aws"
-  version                = "0.5.1"
+  version                = "0.5.2"
   name                   = "security_headers"
   description            = "Adds security headers to the response"
   runtime                = "nodejs12.x"
   lambda_code_source_dir = "${path.module}/../javascript/lambda_at_edge"
-  s3_artifact_bucket     = module.s3website.id
 }
